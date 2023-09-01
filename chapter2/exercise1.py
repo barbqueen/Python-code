db6d02a516f319150d1563a00909a0c062a48146
KLiu <liukevin259@gmail.com>
2023-09-01 10:01:56 -0700

    test

--- chapter2/exercise1.py
+++ chapter2/exercise1.py
@@ -1,1 +1,9 @@
-print('hello')
+name='Kevin'
+address='Canoga Park, CA, 91304'
+telephone_number='626-802-8499'
+college_major='applied math'
+
+print("name: " name)
+print("adress: " address)
+print("telephone number: " telephone number)
+print("college major" college major)
